3d1f3d40b2b770daefad24edc667848905ca3d66
remix1or <remixtor@gmail.com>
2022-09-14 17:35:03 +0300

    Done

--- src/main/java/app/model/Dog.java
+++ src/main/java/app/model/Dog.java
@@ -4,9 +4,9 @@ import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 
 @Component
-public class Cat {
+public class Dog extends Animal{
     @Override
     public String toString() {
-        return "Im a Cat";
+        return "I'm dog";
     }
 }
